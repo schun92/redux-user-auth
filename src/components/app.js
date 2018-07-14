3060b3de5c418865f34d6d675ae6d251751bf074
schun92 <sarahsandiferchun@gmail.com>
2018-07-13 17:46:00 -0700

    completed sign in and pulled movie quotes

--- src/components/app.js
+++ src/components/app.js
@@ -7,6 +7,7 @@ import About from './about';
 import SecretList from './secret_list';
 import MovieQuote from './movie_quote';
 import SignUp from './sign_up';
+import SignIn from './sign_in'
 import auth from '../hoc/auth';
 
 const App = () => (
@@ -19,6 +20,7 @@ const App = () => (
             <Route path="/secret-list" component={auth(SecretList)}/>
             <Route path="/movie-quote" component={MovieQuote}/>
             <Route path="/sign-up" component={SignUp}/>
+            <Route path="/sign-in" component={SignIn}/>
 
         </div>
     </div>
--- src/components/movie_quote.js
+++ src/components/movie_quote.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import auth from '../hoc/auth';
+import { connect } from 'react-redux';
+import { getMovieQuote } from '../actions';
 
-const MovieQuote = props => {
-    return (
-        <div>
-            <h1 className="text-center">Movie Quote</h1>
-            <h4>Just keep swimming...</h4>
-        </div>
-    )
+class MovieQuote extends Component {
+    componentDidMount(){
+        this.props.getMovieQuote();
+    }
+    render() {
+        return (
+            <div>
+                <h1 className="text-center">Movie Quote</h1>
+                <h4>{this.props.quote}</h4>
+            </div>
+        );
+    }
 }
 
-export default auth(MovieQuote);
+function mapStateToProps(state){
+    return{
+        quote: state.movie.quote
+    }
+}
+
+export default connect(mapStateToProps, { getMovieQuote:getMovieQuote })(auth(MovieQuote));
